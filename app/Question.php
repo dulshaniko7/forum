abafd8c5749b09de95d10f43e7fcde8f49c60fc5
abc <abc@example.com>
2020-07-05 20:53:57 +0530

    create forgien

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+     public function user(){
+     	return $this->belongsTo(User::class);
+     }
+
+     public function answers(){
+     	return $this->hasMany(Answer::class);
+     }
+
+     public function category(){
+     	return $this->belongsTo(Category::class);
+     }
+
 }
